ace5d5e6d7fd9eb9644f582df8ab7f31b83a1927
KainAgava <143312166+KainAgava@users.noreply.github.com>
2023-08-27 14:57:35 +0700

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Homework1
+    {
+        static void Main(string[] args)
+        {
+            string necropolis = "Некрополис";
+            string greetingOfTraveler = "Доброе пожаловать в "+ necropolis +" путник ";
+            char cross = '+';
+            int money;
+            int body;
+            int bodyPrice = 10;
+            int goldBank = 100;
+            int boneWarehouse;
+            bool boneStorageCapacity;
+            bool abilityToPay;
+
+
+         
+        }
+    }
+}
